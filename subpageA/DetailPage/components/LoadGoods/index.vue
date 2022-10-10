5ac9c2f4e7b9d5059f30c1ea931cb09a5bebc2c9
duankun <duankun1993@126.com>
2022-10-10 19:02:40 +0800

    预付优化

--- subpageA/DetailPage/components/LoadGoods/index.vue
+++ subpageA/DetailPage/components/LoadGoods/index.vue
@@ -35,9 +35,7 @@
 		mounted() {
 			uni.$on("toOpenSharePoster",()=>{
 				const name=uni.getStorageSync("userInfo").name
-				// const avatar=uni.getStorageSync("userInfo").avatar
 				const avatar=uni.getStorageSync("userInfo").avatar.split("?")[0]
-				// const posterImg = this.goodsData.image
 				const posterImg = this.goodsData.image.split("?")[0]
 				const instanceId = this.goodsData.goodsInstanceId?this.goodsData.goodsInstanceId:""
 				const baseCodeUrl = process.env.NODE_ENV=="development" ? "http://192.168.2.11:8080":"https://h5.jialex.cn"
@@ -47,7 +45,6 @@
 					posterImg,
 					avatar,
 					shopName:this.goodsData.shopName,
-					// shopIcon:this.goodsData.shopIcon,
 					shopIcon:this.goodsData.shopIcon.split("?")[0],
 					goodsName:this.goodsData.goodsName,
 					goodsCode:this.goodsData.goodsCode,

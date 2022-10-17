05ee5ba4017ddca18e2c8ea78287c2c0ba1645fd
duankun <duankun1993@126.com>
2022-10-17 12:28:29 +0800

    v2.0.2

--- subpageA/DetailPage/components/LoadGoods/components/ShowCode/index.vue
+++ subpageA/DetailPage/components/LoadGoods/components/ShowCode/index.vue
@@ -4,8 +4,8 @@
 		<view class="goodsCode_box" v-if="goodsData.loadType==1">
 			<view class="goodsCode">#{{goodsData.goodsCode}}/{{goodsData.totalNumber}}</view>
 		</view>
-		<!-- <view class="" style="width: 160rpx;"></view> -->
-		<donation-and-collection :goodsData="goodsData"/>
+		<view class="" style="width: 160rpx;"></view>
+		<!-- <donation-and-collection :goodsData="goodsData"/> -->
 	</view>
 </template>
 
